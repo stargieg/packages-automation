274a389b2e740f1cd0cd7f9a01fc8a6b0131d40f
Patrick Grimm <patrick@lunatiki.de>
2014-05-24 02:35:57 +0200

    [bacserv] add enable option and more prozess control

--- luci-app-bacnet/luasrc/model/cbi/bacnet/bacnet_dev.lua
+++ luci-app-bacnet/luasrc/model/cbi/bacnet/bacnet_dev.lua
@@ -12,24 +12,57 @@ You may obtain a copy of the License at
 $Id$
 ]]--
 
-require("luci.sys")
-require("luci.util")
-require("luci.tools.webadmin")
-require("luci.fs")
-local uci = luci.model.uci.cursor()
-local uci_state = luci.model.uci.cursor_state()
+local fs  = require "nixio.fs"
+local lfs  = require "luci.fs"
+local uci = require "luci.model.uci".cursor()
+local sys = require "luci.sys"
+local uci_state = require "luci.model.uci".cursor_state()
 
-if not luci.fs.access("/etc/config/bacnet_dev") then
-	if not luci.sys.exec("touch /etc/config/bacnet_dev") then
+if not lfs.access("/etc/config/bacnet_dev") then
+	if not sys.exec("touch /etc/config/bacnet_dev") then
 		return
 	end
 end
 
-m = Map("bacnet_dev", "Bacnet Device", "Bacnet Device Configuration")
+local m = Map("bacnet_dev", "Bacnet Device", "Bacnet Device Configuration")
 
-s = m:section(TypedSection, "dev", 'Device Nummer')
+local s = m:section(TypedSection, "dev", 'Device Nummer')
 s.addremove = true
 s.anonymous = false
+
+local active = s:option(DummyValue, "_active", translate("Started") )
+function active.cfgvalue(self, section)
+	local pid = fs.readfile("/var/run/bacserv-%s.pid" % section)
+	if pid and #pid > 0 and tonumber(pid) ~= nil then
+		return (sys.process.signal(pid, 0))
+			and translatef("yes (%i)", pid)
+			or  translate("no")
+	end
+	return translate("no").." "..section
+end
+
+local updown = s:option(Button, "_updown", translate("Start/Stop") )
+updown._state = false
+function updown.cbid(self, section)
+	local pid = fs.readfile("/var/run/bacserv-%s.pid" % section)
+	self._state = pid and #pid > 0 and sys.process.signal(pid, 0)
+	self.option = self._state and "stop" or "start"
+	
+	return AbstractValue.cbid(self, section)
+end
+function updown.cfgvalue(self, section)
+	self.title = self._state and "stop" or "start"
+	self.inputstyle = self._state and "reset" or "reload"
+end
+function updown.write(self, section, value)
+	if self.option == "stop" then
+		sys.call("/etc/init.d/bacserv stop %s" % section)
+	else
+		sys.call("/etc/init.d/bacserv start %s" % section)
+	end
+end
+
+s:option(Flag, "enable", "enable")
 
 sva = s:option(Value, "bacdl", "Netzwerk Layer")
 sva:value('bip','BACnet IPv4')
