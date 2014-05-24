ff9bfbb975b217068bd51ea099e95161b4c78674
Patrick Grimm <patrick@lunatiki.de>
2014-05-24 03:09:08 +0200

    [luci-app-bacnet] add bacnet menu entry

--- luci-app-bacnet/luasrc/controller/bacnet.lua
+++ luci-app-bacnet/luasrc/controller/bacnet.lua
@@ -17,32 +17,33 @@ function index()
 	if not nixio.fs.access("/etc/config/bacnet_dev") then
 		return
 	end
-	local page = entry({"admin", "services", "bacnet_dev"}, cbi("bacnet/bacnet_dev"), "Bacnet Device", 20)
+	entry({"admin", "bacnet"}, alias("admin", "bacnet", "bacnet_dev"), _("Bacnet"), 40).index = true
+	local page = entry({"admin", "bacnet", "bacnet_dev"}, cbi("bacnet/bacnet_dev"), "Bacnet Device", 20)
 	page.dependent = true
-	local page = entry({"admin", "services", "bacnet_ai"}, cbi("bacnet/bacnet_ai"), "Analog Input", 21)
+	local page = entry({"admin", "bacnet", "bacnet_ai"}, cbi("bacnet/bacnet_ai"), "Analog Input", 21)
 	page.dependent = true
-	local page = entry({"admin", "services", "bacnet_ao"}, cbi("bacnet/bacnet_ao"), "Analog Output", 22)
+	local page = entry({"admin", "bacnet", "bacnet_ao"}, cbi("bacnet/bacnet_ao"), "Analog Output", 22)
 	page.dependent = true
-	local page = entry({"admin", "services", "bacnet_av"}, cbi("bacnet/bacnet_av"), "Analog Value", 23)
+	local page = entry({"admin", "bacnet", "bacnet_av"}, cbi("bacnet/bacnet_av"), "Analog Value", 23)
 	page.dependent = true
-	local page = entry({"admin", "services", "bacnet_bi"}, cbi("bacnet/bacnet_bi"), "Binary Input", 24)
+	local page = entry({"admin", "bacnet", "bacnet_bi"}, cbi("bacnet/bacnet_bi"), "Binary Input", 24)
 	page.dependent = true
-	local page = entry({"admin", "services", "bacnet_bo"}, cbi("bacnet/bacnet_bo"), "Binary Output", 25)
+	local page = entry({"admin", "bacnet", "bacnet_bo"}, cbi("bacnet/bacnet_bo"), "Binary Output", 25)
 	page.dependent = true
-	local page = entry({"admin", "services", "bacnet_bv"}, cbi("bacnet/bacnet_bv"), "Binary Value", 26)
+	local page = entry({"admin", "bacnet", "bacnet_bv"}, cbi("bacnet/bacnet_bv"), "Binary Value", 26)
 	page.dependent = true
-	local page = entry({"admin", "services", "bacnet_mi"}, cbi("bacnet/bacnet_mi"), "Multisate Input", 27)
+	local page = entry({"admin", "bacnet", "bacnet_mi"}, cbi("bacnet/bacnet_mi"), "Multisate Input", 27)
 	page.dependent = true
-	local page = entry({"admin", "services", "bacnet_mo"}, cbi("bacnet/bacnet_mo"), "Multisate Output", 28)
+	local page = entry({"admin", "bacnet", "bacnet_mo"}, cbi("bacnet/bacnet_mo"), "Multisate Output", 28)
 	page.dependent = true
-	local page = entry({"admin", "services", "bacnet_mv"}, cbi("bacnet/bacnet_mv"), "Multisate Value", 29)
+	local page = entry({"admin", "bacnet", "bacnet_mv"}, cbi("bacnet/bacnet_mv"), "Multisate Value", 29)
 	page.dependent = true
-	local page = entry({"admin", "services", "bacnet_nc"}, cbi("bacnet/bacnet_nc"), "Notification Class", 30)
+	local page = entry({"admin", "bacnet", "bacnet_nc"}, cbi("bacnet/bacnet_nc"), "Notification Class", 30)
 	page.dependent = true
 
-	local page = entry({"admin", "services", "bacnetgroups"}, cbi("bacnet/groups", {autoapply=false}), "Bacnet Groups", 29)
+	local page = entry({"admin", "bacnet", "bacnetgroups"}, cbi("bacnet/groups", {autoapply=false}), "Bacnet Groups", 29)
 	page.dependent = true
-	local page = entry({"admin", "services", "icinga_mv"}, cbi("bacnet/icinga_mv"), "Icinga MV", 30)
+	local page = entry({"admin", "bacnet", "icinga_mv"}, cbi("bacnet/icinga_mv"), "Icinga MV", 30)
 	page.dependent = true
 end
 
