4ca3c209a70408af329fb875670d41f97f95306c
Patrick Grimm <patrick@lunatiki.de>
2016-01-04 00:07:26 +0100

    knxd: add def. mcast route, cleanup help texts

--- luci-app-knxd/luasrc/model/cbi/knxd/knxd.lua
+++ luci-app-knxd/luasrc/model/cbi/knxd/knxd.lua
@@ -13,31 +13,50 @@ $Id$
 ]]--
 
 require("luci.tools.webadmin")
+local uci = luci.model.uci.cursor()
 
-m = Map("knxd", "KNX Server", "KNX Server for RS232 USB EIB/IP Routing EIB/IP Tunnelling")
+m = Map("knxd", "KNX Server", "KNX Server for RS232, USB, EIB/IP Routing and EIB/IP Tunnelling")
 
-s = m:section(TypedSection, "eibinterface", "KNX Interface")
-s.addremove = true
-s.anonymous = true
-s:option(DummyValue, "dv1", "info", "Supported Hardware: http://sourceforge.net/apps/trac/bcusdk/wiki/SupportedHardware")
-s:option(DummyValue, "dv1", "info", "FAQ: http://sourceforge.net/apps/trac/bcusdk/wiki/FAQ")
+s = m:section(NamedSection, "args", "KNX Interface")
+s:option(DummyValue, "dv1", "info", "Supported Hardware: https://web.archive.org/web/20140331121456/http://sourceforge.net/apps/trac/bcusdk/wiki/SupportedHardware")
+s:option(DummyValue, "dv1", "info", "FAQ: https://web.archive.org/web/20120917203923/http://sourceforge.net/apps/trac/bcusdk/wiki/FAQ")
 
 s:option(DummyValue, "dv1", "supported URLs are:")
-s:option(DummyValue, "dv1", "ft12:/dev/ttySx", "FT1.2 Protocol to a BCU 2")
-s:option(DummyValue, "dv1", "bcu1:/dev/eib", "(using a kernel driver) opkg install kmod-bcu1driver")
-s:option(DummyValue, "dv1", "tpuart24:/dev/tpuartX", "(using the TPUART driver, Linux Kernel 2.4) no opkg")
-s:option(DummyValue, "dv1", "tpuart:/dev/tpuartX", "(using the TPUART driver, Linux Kernel 2.6) no opkg")
 s:option(DummyValue, "dv1", "ip:[multicast_addr[:port]]", "connects with the EIBnet/IP Routing protocol")
 s:option(DummyValue, "dv1", "ipt:router_ip[:dest_port[:src_port[:nat_ip[:data_port]]]]]", "connects with the EIBnet/IP Tunneling protocol")
 s:option(DummyValue, "dv1", "iptn:router_ip[:dest_port[:src_port]]", "connects with the EIBnet/IP Tunneling protocol over an EIBnet/IP gateway using the NAT mode")
-s:option(DummyValue, "dv1", "bcu1s:/dev/ttySx", "connects using the PEI16 Protocoll over a BCU experimental")
-s:option(DummyValue, "dv1", "tpuarts:/dev/ttySx", "connects to the KNX bus over an TPUART experimental")
-s:option(DummyValue, "dv1", "usb:[bus[:device[:config[:interface]]]]", "default is autodetect with findknxusb tool connects over a KNX USB interface autodetect with findknxusb tool")
+s:option(DummyValue, "dv1", "tpuarts:/dev/ttyACM0", "connects to the KNX bus over a TPUART (using a serial interface)")
+s:option(DummyValue, "dv1", "usb:[bus[:device[:config[:interface]]]]", "connects over a KNX USB interface")
 
 svc = s:option(Value, "url", "URL")
 svc:value("usb:")
 svc:value("ip:")
 svc:value("tpuarts:/dev/ttyACM0")
+function svc.validate(self, value, section)
+	if string.find(value, 'ip:') then
+		local mcast=string.match(value, "(%d+.%d+.%d+.%d+)") or "224.0.23.12"
+		local route
+		uci:foreach("network", "route", function(g)
+			if g.target==mcast then
+				route=true
+			end
+		end)
+		if route then
+			return value
+		else
+			uci:section("network", "route", nil, {
+				gateway = "0.0.0.0",
+				interface="lan",
+				target=mcast
+			})
+			uci:save("network")
+			return nil, "Save and Apply the Multicast Route "...mcast.." in the Networkconfig"
+		end
+	else
+		return value
+	end
+end
+
 
 s:option(Flag, "Discovery", "Discover for ETS").optional = true
 s:option(Flag, "Server", "Server for ETS").optional = true
