1411c55c242149a701c5a98a74b2df754098e75d
Patrick Grimm <patrick@lunatiki.de>
2016-01-13 00:37:51 +0100

    luci-app-knxd: add diag view

--- luci-app-knxd/luasrc/controller/knxd.lua
+++ luci-app-knxd/luasrc/controller/knxd.lua
@@ -15,15 +15,80 @@ $Id$
 module("luci.controller.knxd", package.seeall)
 
 function index()
---	require("luci.i18n")
---	luci.i18n.loadc("knxd")
-	if not nixio.fs.access("/etc/config/knxd") then
+	local uci = require("luci.model.uci").cursor()
+	local fs = require("nixio.fs")
+
+	if not fs.access("/etc/config/knxd") then
 		return
 	end
 
-	local page = entry({"admin", "services", "knxd"}, cbi("knxd"), "knxd")
---	page.i18n = "knxd"
+	local page
+	page = entry({"admin", "services", "knxd"}, cbi("knxd"), "knxd")
 	page.dependent = true
 	page.order = 10
 
+	page = node("admin", "services", "knxd_diag")
+	page.target = template("knxd_diag")
+	page.title  = _("KNX Diagnostics")
+	page.order  = 11
+
+	--page = entry({"admin", "services", "knxd_diag_vbusmonitor"}, post("knxd_diag_vbusmonitor"), nil)
+	--page.leaf = true
+
+	--page = entry({"admin", "services", "knxd_diag_groupsocketlisten"}, post("knxd_diag_groupsocketlisten"), nil)
+	--page.leaf = true
+
+	--page = entry({"admin", "services", "knxd_diag_groupswrite"}, post("knxd_diag_groupswrite"), nil)
+	--page.leaf = true
+end
+
+function knxd_diag_vbusmonitor()
+	local cmd = "knxtool vbusmonitor1 ip:127.0.0.1 2>&1"
+	luci.http.prepare_content("text/plain")
+	luci.http.write(cmd)
+	local util = io.popen(cmd)
+	if util then
+		while true do
+			local ln = util:read("*l")
+			if not ln then break end
+			luci.http.write(ln)
+			luci.http.write("\n")
+		end
+		util:close()
+	end
+	return
+end
+
+function knxd_diag_groupsocketlisten()
+	local cmd = "knxtool groupsocketlisten ip:127.0.0.1 2>&1"
+	luci.http.prepare_content("text/plain")
+	luci.http.write(cmd)
+	local util = io.popen(cmd)
+	if util then
+		while true do
+			local ln = util:read("*l")
+			if not ln then break end
+			luci.http.write(ln)
+			luci.http.write("\n")
+		end
+		util:close()
+	end
+	return
+end
+
+function knxd_diag_groupswrite(addr,value)
+	local cmd = "knxtool groupswrite ip:127.0.0.1 "..addr.." "..value.." 2>&1"
+	luci.http.prepare_content("text/plain")
+	luci.http.write(cmd)
+	local util = io.popen(cmd)
+	if util then
+		while true do
+			local ln = util:read("*l")
+			if not ln then break end
+			luci.http.write(ln)
+			luci.http.write("\n")
+		end
+		util:close()
+	end
+	return
 end
